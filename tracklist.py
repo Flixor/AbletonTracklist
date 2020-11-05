2e5db7b972021af95eed44454d735ed65aea13a6
Felix <felixhopostma@gmail.com>
2020-11-05 20:00:18 +0100

    .als selection works, subprocess gzip does not, use python unzip?

--- tracklist.py
+++ tracklist.py
@@ -1,22 +1,28 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 ### Felix Postma 2020
 
+import subprocess
+from tkinter import *
 from tkinter import filedialog
+import subprocess
 from xml.etree import ElementTree as ET
 
+
+# initialise dialog window
 tk = Tk()
 tk.title('Find ALS')
 
-# how to get to directory?
+# choose .als
+tk.filename = filedialog.askopenfilename(initialdir="/Users/flixor/Projects/AbletonTracklist", title="Select A File", filetypes=(("Ableton session files", "*.als"),("all files", "*.*")))
+# tk.mainloop() not necessary!
+print(tk.filename)
 
-tk.filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("Ableton session files", "*.als"),("all files", "*.*")))
-
-tk.mainloop()
+# /Users/flixor/Projects/AbletonTracklist/test.als
 
 # gzip to make xml
-
+# p = subprocess.run(["gzip", "-cd", tk.filename], capture_output=True, shell=True)
 
 # get xml elements
 
